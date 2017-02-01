ce99608e24e28cae265f9b70874f639eec9d602f
Eduardo Galego <cayanvieira@EDUARDO-MAC.local>
2017-02-01 10:47:10 -0200

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,224 @@
+
+//Dropdown Menus
+$(".dropdown").hover(
+  function () {
+    $(this).addClass('open');
+  }, 
+  function () {
+    $(this).removeClass('open');
+  }
+  );
+
+
+//WOW Scroll Spy
+var wow = new WOW({
+    //disabled for mobile
+    mobile: false
+});
+wow.init();
+
+
+//Owl Carousel
+$('#clients-scroller').owlCarousel({
+    items:4,
+    itemsTablet:3,
+    margin:90,
+    stagePadding:90,
+    smartSpeed:450,
+    itemsDesktop : [1199,4],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [768,3],
+    itemsTablet: [767,2],
+    itemsTabletSmall: [480,2],
+    itemsMobile : [479,1],
+});
+
+//Color Client
+$('#color-client-scroller').owlCarousel({
+    items:4,
+    itemsTablet:3,
+    margin:90,
+    stagePadding:90,
+    smartSpeed:450,
+    itemsDesktop : [1199,4],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [768,3],
+    itemsTablet: [767,2],
+    itemsTabletSmall: [480,2],
+    itemsMobile : [479,1],
+});
+
+//Owl Carousel
+$('#testimonial-item').owlCarousel({
+    autoPlay: 5000,
+    items:3,
+    itemsTablet:3,
+    margin:90,
+    stagePadding:90,
+    smartSpeed:450,
+    itemsDesktop : [1199,4],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [768,3],
+    itemsTablet: [767,2],
+    itemsTabletSmall: [480,2],
+    itemsMobile : [479,1],
+});
+
+//Dark Testimonial Carousel
+$('#testimonial-dark').owlCarousel({
+    autoPlay: 5000,
+    items:3,
+    itemsTablet:3,
+    margin:90,
+    stagePadding:90,
+    smartSpeed:450,
+    itemsDesktop : [1199,4],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [768,3],
+    itemsTablet: [767,2],
+    itemsTabletSmall: [480,2],
+    itemsMobile : [479,1],
+});
+
+// Single Testimonial
+$('#single-testimonial-item').owlCarousel({
+  singleItem: true,
+  autoPlay: 5000,
+    items: 1,
+    itemsTablet: 1,
+    margin:90,
+    stagePadding:90,
+    smartSpeed:450,
+    itemsDesktop : [1199,4],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [768,3],
+    itemsTablet: [767,2],
+    itemsTabletSmall: [480,2],
+    itemsMobile : [479,1],
+    stopOnHover: true,
+});
+
+// Image Carousel
+$("#image-carousel").owlCarousel({
+  autoPlay: 3000, //Set AutoPlay to 3 seconds
+  items : 4,
+  itemsDesktop : [1170,3],
+  itemsDesktopSmall : [1170,3]
+ 
+});
+
+// Slider Carousel
+$("#carousel-image-slider").owlCarousel({
+  navigation : false, // Show next and prev buttons
+  slideSpeed : 300,
+  paginationSpeed : 400,
+  singleItem:true,
+  pagination: false,
+  autoPlay: 3000,
+});
+
+
+ //About owl carousel Slider
+  $(document).ready(function(){
+    /*=== About us ====*/
+    $('#carousel-about-us').owlCarousel({   
+        navigation: true, // Show next and prev buttons
+        navigationText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        slideSpeed: 800,
+        paginationSpeed: 400,
+        autoPlay: true,
+        singleItem: true,
+        pagination : false,
+        items : 1,
+        itemsCustom : false,
+        itemsDesktop : [1199,4],
+        itemsDesktopSmall : [980,3],
+        itemsTablet: [768,2],
+        itemsTabletSmall: false,
+        itemsMobile : [479,1],
+    });
+
+});
+
+//MixitUp
+$(function(){
+  $('#portfolio-list').mixItUp();
+});
+
+// Testimonial
+$('testimonial-carousel').carousel();
+$('a[data-slide="prev"]').click(function () {
+    $('#testimonial-carousel').carousel('prev');
+});
+
+$('a[data-slide="next"]').click(function () {
+    $('#testimonial-carousel').carousel('next');
+});
+
+//CounterUp
+  jQuery(document).ready(function( $ ) {
+      $('.counter').counterUp({
+          delay: 1,
+          time: 800
+      });
+  });
+
+// Progress Bar
+$('.skill-shortcode').appear(function() {
+  $('.progress').each(function() {
+    $('.progress-bar').css('width', function() {
+      return ($(this).attr('data-percentage') + '%')
+    });
+  });
+}, {
+  accY: -100
+});
+
+
+ // Back Top Link
+  var offset = 200;
+  var duration = 500;
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > offset) {
+      $('.back-to-top').fadeIn(400);
+    } else {
+      $('.back-to-top').fadeOut(400);
+    }
+  });
+  $('.back-to-top').click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, 600);
+    return false;
+  })
+
+
+  // Add scrollspy to <body>
+$('body').scrollspy({target: ".navbar", offset: 50});
+
+// Add smooth scrolling on all links inside the navbar
+$("#myNavbar a").on('click', function(event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+
+  } // End if
+
+});
